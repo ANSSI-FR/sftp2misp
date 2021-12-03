ce739c6fd0c6f47ebdd9ccd2246135ddbcb88544
Victor Barrault <vbarrault@vbarrault.mrc..cossi.internet>
2021-12-03 15:39:12 +0100

    Ajout d'une option pour spécifier un fichier de configuration dans la ligne de commande

--- config.py
+++ config.py
@@ -2,31 +2,42 @@ import yaml
 import logging, logging.config
 import os
 
-def get_config():
-    with open("conf/config.yaml", "r") as config_file:
-        return yaml.safe_load(config_file)
+def get_config(conf_file):
+    with open(conf_file, "r") as config_file:
+        conf = yaml.safe_load(config_file)
+        return conf["SFTP"], conf["MISP"], conf["MISC"]
 
-config = get_config()
-sftp_c=config["SFTP"]
-misp_c=config["MISP"]
-misc_c=config["MISC"]
 
-def get_logging_config():
-    with open(misc_c["logging_conf"], "r") as config_file:
-        return yaml.safe_load(config_file)
-def get_logger():
+def get_logging_config(log_conf_file):
+    with open(log_conf_file, "r") as log_conf:
+        return yaml.safe_load(log_conf)
+
+
+def get_logger(log_conf_file, log_file):
     try:
-        os.mkdir("./log")
+        os.mkdir(os.path.dirname(log_file))
     except FileExistsError:
         pass
     except:
         print("Unexpected error: ", sys.exc_info()[0])
         raise
-    logging.config.dictConfig(get_logging_config()["LOGGING"])
-    return logging.getLogger(__name__)
+    logging.config.dictConfig(get_logging_config(log_conf_file)["LOGGING"])
+    file_handler = logging.handlers.RotatingFileHandler(log_file, 'a',
+                            maxBytes=10485760, backupCount=20,
+                            encoding="utf8")
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s.%(funcName)s(): %(message)s')
+    file_handler.setFormatter(formatter)
 
-if(misp_c["ssl"]):
-    os.environ['REQUESTS_CA_BUNDLE'] = CONFIG["MISP_CONF"]["CA_BUNDLE"]
+    logger = logging.getLogger()
+    for hdlr in logger.handlers[:]:
+        if isinstance(hdlr,logging.FileHandler):
+            logger.removeHandler(hdlr)
+    logger.addHandler(file_handler)
+    logger.info('Chargement du fichier de configuration terminé')
+    return logger
 
-logger = get_logger()
-logger.info('Chargement du fichier de configuration terminé')
+
+def set_ssl(misp_c):
+    if(misp_c["ssl"]):
+        os.environ['REQUESTS_CA_BUNDLE'] = CONFIG["MISP_CONF"]["CA_BUNDLE"]
