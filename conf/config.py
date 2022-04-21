2b64a772d4228b8196db03805cc22f580cad04c1
Victor Barrault <vbarrault@vbarrault.mrc..cossi.internet>
2022-04-21 16:47:55 +0200

    formatting using black

--- conf/config.py
+++ conf/config.py
@@ -30,16 +30,18 @@ def get_logger(log_conf_file, log_directory, log_suffix):
         log_directory += "/"
     filename = now.strftime(f"{log_directory}%Y%m%d_{log_suffix}")
     logging.config.dictConfig(get_logging_config(log_conf_file)["LOGGING"])
-    file_handler = logging.handlers.RotatingFileHandler(filename, 'a',
-                            maxBytes=10485760, backupCount=20,
-                            encoding="utf8")
+    file_handler = logging.handlers.RotatingFileHandler(
+        filename, "a", maxBytes=10485760, backupCount=20, encoding="utf8"
+    )
     file_handler.setLevel(1)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s.%(funcName)s(): %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(name)s.%(funcName)s(): %(message)s"
+    )
     file_handler.setFormatter(formatter)
 
     logger = logging.getLogger()
     for hdlr in logger.handlers[:]:
-        if isinstance(hdlr,logging.FileHandler):
+        if isinstance(hdlr, logging.FileHandler):
             logger.removeHandler(hdlr)
     logger.addHandler(file_handler)
     logger.info("Configuration file loading completed")
@@ -47,5 +49,5 @@ def get_logger(log_conf_file, log_directory, log_suffix):
 
 
 def set_ssl(misp_c):
-    if(misp_c["ssl"]):
-        os.environ['REQUESTS_CA_BUNDLE'] = misp_c["CA_BUNDLE"]
+    if misp_c["ssl"]:
+        os.environ["REQUESTS_CA_BUNDLE"] = misp_c["CA_BUNDLE"]
