dc12196ec75e2b1e78829a1fdd5ef33e46f81452
Victor Barrault <vbarrault@vbarrault.mrc..cossi.internet>
2022-04-27 15:51:05 +0200

    black

--- conf/_TimestampedRotatingFileHandler.py
+++ conf/_TimestampedRotatingFileHandler.py
@@ -7,9 +7,21 @@ import os
 import logging
 from datetime import datetime
 
+
 class TimestampedRotatingFileHandler(logging.handlers.RotatingFileHandler):
-    def __init__(self, filename, mode='a', maxBytes=0, date="%Y%m%d", backupCount=0, encoding=None, delay=False):
+    def __init__(
+        self,
+        filename,
+        mode="a",
+        maxBytes=0,
+        date="%Y%m%d",
+        backupCount=0,
+        encoding=None,
+        delay=False,
+    ):
         filename, extension = os.path.splitext(filename)
         path, filename = os.path.split(filename)
         filename = f"{path}/{datetime.now().strftime(date)}_{filename}{extension}"
-        logging.handlers.RotatingFileHandler.__init__(self, filename, mode, maxBytes, backupCount, encoding, delay)
+        logging.handlers.RotatingFileHandler.__init__(
+            self, filename, mode, maxBytes, backupCount, encoding, delay
+        )
