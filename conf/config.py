aea7b9a8b5622c12b82716ce834019e300a0a7ab
Victor Barrault <victor.barrault@ssi.gouv.fr>
2022-09-07 16:50:46 +0200

    v1.1.1

--- conf/config.py
+++ conf/config.py
@@ -1,54 +1,109 @@
+"""
+Configuration module for sftp2misp script
+"""
 import logging
 import logging.config
 import os
 import sys
-import datetime
 import yaml
+from pathlib import Path
 
 
-def get_config(conf_file):
-    with open(conf_file, "r") as config_file:
-        conf = yaml.safe_load(config_file)
-        return conf["SFTP"], conf["MISP"], conf["MISC"]
+def check_config(config):
+    """
+    Check compliance of given config file
+    """
+    sftp_entries = {
+        "host": str,
+        "port": int,
+        "sftp_directories": list,
+        "username": str,
+        "private_key_file": str,
+        "proxy_command": str,
+        "proxy_host": str,
+        "proxy_port": int,
+    }
+    misp_entries = {
+        "url": str,
+        "key": str,
+        "bypass_proxy": bool,
+        "ssl": bool,
+        "CA_BUNDLE": str,
+    }
+    misc_entries = {
+        "local_directory": str,
+        "logging_conf": str
+    }
+    check_entries = [sftp_entries, misp_entries, misc_entries]
+    config_entries = ["SFTP", "MISP", "MISC"]
+    error = []
+    for i, group in enumerate(check_entries):
+        for entry in group:
+            if entry not in config[config_entries[i]]:
+                error.append(f"\tentry \"{entry}\" of config.yaml.template is not in your config file")
+            elif type(config[config_entries[i]][entry]) != group[entry]:
+                error.append(
+                    f"\tentry \"{entry}\" has type {type(config[config_entries[i]][entry])} but needs to be of type {group[entry]}"
+                )
+        for entry in config[config_entries[i]]:
+            if not entry in group:
+                error.append(f"\tentry \"{entry}\" of your config file is not in config.yaml.template")
+    return error
 
 
-def get_logging_config(log_conf_file):
-    with open(log_conf_file, "r") as log_conf:
-        return yaml.safe_load(log_conf)
-
-
-def get_logger(log_conf_file, log_directory, log_suffix):
+def get_config(config_file):
+    """
+    Open, load and check for errors in configuration file
+    """
+    config_file = Path(config_file).resolve()
     try:
-        os.mkdir(log_directory)
-    except FileExistsError:
-        pass
-    except:
-        print("Unexpected error: ", sys.exc_info()[0])
-        raise
-    now = datetime.datetime.now()
-    if log_directory[-1] != "/":
-        log_directory += "/"
-    filename = now.strftime(f"{log_directory}%Y%m%d_{log_suffix}")
-    logging.config.dictConfig(get_logging_config(log_conf_file)["LOGGING"])
-    file_handler = logging.handlers.RotatingFileHandler(
-        filename, "a", maxBytes=10485760, backupCount=20, encoding="utf8"
-    )
-    file_handler.setLevel(1)
-    formatter = logging.Formatter(
-        "%(asctime)s - %(levelname)s - %(name)s.%(funcName)s(): %(message)s"
-    )
-    file_handler.setFormatter(formatter)
+        with open(config_file, "r") as config_file:
+            conf = yaml.safe_load(config_file)
+            errors = check_config(conf)
+            if errors:
+                entry = ["List of entries", "Entry"]
+                print(
+                    f"Your configuration file {config_file.name} is not compliant with config.yaml.template : entries are missing or not supported. Please review and fix it."
+                )
+                print(f"{entry[len(errors) == 1]} not compliant :")
+                for error in errors:
+                    print(error)
+                sys.exit(1)
+            return conf["SFTP"], conf["MISP"], conf["MISC"]
+    except FileNotFoundError as _:
+        print(f"Configuration file \"{config_file}\" not found")
+        sys.exit(1)
 
-    logger = logging.getLogger()
-    for hdlr in logger.handlers[:]:
-        if isinstance(hdlr, logging.FileHandler):
-            logger.removeHandler(hdlr)
-    logger.addHandler(file_handler)
-    logger.info("Configuration file loading completed")
-    logging.captureWarnings(True)
-    return logger
 
+
+def create_logger(config_log):
+    """
+    Open and load configuration file for logging
+    """
+    logging_path = Path(__file__).parent.parent / Path(config_log["logging_conf"])
+    try:
+        with open(logging_path, "r") as log_conf_file:
+            log_conf = yaml.safe_load(log_conf_file)
+            path, _ = os.path.split(log_conf["LOGGING"]["handlers"]["file"]["filename"])
+            try:
+                os.mkdir(path)
+            except FileExistsError:
+                pass
+            except:
+                print("Unexpected error: ", sys.exc_info()[0])
+                raise
+            logging.config.dictConfig(log_conf["LOGGING"])
+            logger = logging.getLogger("__name__")
+            logger.info("Configuration file loading completed")
+            logging.captureWarnings(True)
+            return logger
+    except FileNotFoundError as _:
+        print(f"Configuration file \"{logging_path}\" not found")
+        sys.exit(1)
 
 def set_ssl(misp_c):
+    """
+    Set environnment variable in order to use https communication
+    """
     if misp_c["ssl"]:
         os.environ["REQUESTS_CA_BUNDLE"] = misp_c["CA_BUNDLE"]
